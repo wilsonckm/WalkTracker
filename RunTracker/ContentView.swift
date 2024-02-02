af7d4bcc104bd51a3d6cff1168f80292f56eb59e
wilsonckm <wilsonckmchan@gmail.com>
2024-02-02 14:09:13 -0500

    Initial Commit

---
+++ RunTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunTracker
+//
+//  Created by Wilson Chan on 2/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
